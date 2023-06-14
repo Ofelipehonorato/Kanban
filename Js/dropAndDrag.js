3085a2594272572e87edb24cfbeb3e465567eaa9
Felipe Franciscato Honorato <fefranciscato@gmail.com>
2023-06-14 17:08:29 -0300

    Finalização da troca de cores dos cards

--- Js/dropAndDrag.js
+++ Js/dropAndDrag.js
@@ -3,12 +3,11 @@ function log(message) {
   console.log('> ' + message)
 }
 
-/** app */
 const cards = document.querySelectorAll('.card')
 const dropzones = document.querySelectorAll('.dropzone')
 
 
-/** our cards */
+// Funções do drag
 cards.forEach(card => {
   card.addEventListener('dragstart', dragstart)
   card.addEventListener('drag', drag)
@@ -16,26 +15,20 @@ cards.forEach(card => {
 })
 
 function dragstart() {
-  // log('CARD: Start dragging ')
   dropzones.forEach( dropzone => dropzone.classList.add('highlight'))
-
-  // this = card
   this.classList.add('is-dragging')
 }
 
 function drag() {
-  // log('CARD: Is dragging ')
 }
 
 function dragend() {
-  // log('CARD: Stop drag! ')
   dropzones.forEach( dropzone => dropzone.classList.remove('highlight'))
 
-  // this = card
   this.classList.remove('is-dragging')
 }
 
-/** place where we will drop cards */
+
 dropzones.forEach( dropzone => {
   dropzone.addEventListener('dragenter', dragenter)
   dropzone.addEventListener('dragover', dragover)
@@ -43,55 +36,65 @@ dropzones.forEach( dropzone => {
   dropzone.addEventListener('drop', drop)
 })
 
-function dragenter() {
-  // log('DROPZONE: Enter in zone ')
-}
+function dragenter() {}
 
 function dragover() {
-  // this = dropzone
   this.classList.add('over')
 
-  // get dragging card
+  // Card Capturado
   const cardSelected = document.querySelector('.is-dragging')
-
-  // this = dropzone
   this.appendChild(cardSelected)
 
   const legenda = this.querySelector(".legend");
 
-  //Pegar todo o estilo carregado da linha acima(no caso, pega a class e assim o backcolor)
   const style = window.getComputedStyle(legenda);
 
-  /*
-    Pega todo o elemento HTML onde esta a class status
-    <div class="status green">
-        <p>Sprint Meet</p>
-    </div>
-    */
-  const status = cardSelected.querySelector(".status");
 
-  //Dependendo do background encontrado, seta a cor atual e remove as outras.
-  if (style.backgroundColor === "rgb(119,171,189)") {
+  const status = cardSelected.querySelector(".status");
+  
+  if (style.backgroundColor === "rgba(119, 171, 189, 0.01)") { // finalizacao
     status.classList.remove("red");
     status.classList.remove("blue");
     status.classList.remove("darkblue");
     status.classList.add("lightblue");
-  } else if (style.backgroundColor === "rgb(69,123,157)") {
-    status.classList.remove("blue");
-    status.classList.remove("darkblue");
+  } else if (style.backgroundColor === "rgba(69, 123, 157, 0.01)") { // revisao
     status.classList.remove("red");
+    status.classList.remove("blue");
+    status.classList.remove("lightblue");
     status.classList.add("darkblue");
-  } else if (style.backgroundColor === "rgb(29,53,87)") {
+  } else if (style.backgroundColor === "rgba(29, 53, 87, 0.01)") { // execucao
     status.classList.remove("red");
     status.classList.remove("lightblue");
     status.classList.remove("darkblue");
     status.classList.add("blue");
-  } else if (style.backgroundColor === "rgb(230,57,70)") {
+  } else if (style.backgroundColor === "rgba(230, 57, 71, 0.01)") { // afazer
     status.classList.remove("blue");
-    status.classList.remove("lightblue");
     status.classList.remove("darkblue");
+    status.classList.remove("lightblue");
     status.classList.add("red");
   }
+
+  // const btnAfazer = document.getElementById("btnAfazer")
+  // const btnExecucao = document.getElementById("btnExecucao")
+  // const btnRevisao = document.getElementById("btnRevisao")
+  // const btnFim = document.getElementById("btnFim")
+  // // const iconX = document.getElementById("iconX")
+  // // const iconMaior = document.getElementById("iconMaior")
+
+  // if (style.backgroundColor === "rgba(119, 171, 189, 0.01)") { // finalizacao
+  //   btnFim.setAttribute('src', './img/icons/close-fim.png')
+  //   // iconMaior.src = "./img/icons/maximize-fim.png"
+  // } else if (style.backgroundColor === "rgba(69, 123, 157, 0.01)") { // revisao
+  //   btnRevisao.setAttribute('src','./img/icons/close-revisao.png')
+  //   // iconMaior.src = "./img/icons/maximize-revisao.png"
+  // } else if (style.backgroundColor === "rgba(29, 53, 87, 0.01)") { // execucao
+  //   btnExecucao.setAttribute('src','/img/icons/close-execu.png')
+  //   // iconMaior.src = "./img/icons/maximize-execu.png"
+  // } else if (style.backgroundColor === "rgba(230, 57, 71, 0.01)") { // afazer
+  //   btnAfazer.setAttribute('src', './img/icons/close-fazer.png')
+  //   // iconMaior.src = "./img/icons/maximize-fazer.png"
+  // }
+
   
 } 
 
@@ -105,5 +108,5 @@ function dragleave() {
 
 function drop() {
   // log('DROPZONE: dropped ')
-  // this.classList.remove('over')
+  this.classList.remove('over')
 }
