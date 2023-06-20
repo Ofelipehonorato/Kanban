ae9973beca4890be0d4b1309e905d42e019adeb8
Felipe Franciscato Honorato <fefranciscato@gmail.com>
2023-06-20 17:26:11 -0300

    Criação do popup ao clicar no btn de adicionar

--- Js/button.js
+++ Js/button.js
@@ -1,36 +1,96 @@
+function openPopup() {
+    let popup = document.getElementById("popup");
+    popup.style.display = "block";
+  }
+
 import { dragstart, drag, dragend, drop } from "./dropAndDrag.js";
 
-const btnAdd = document.querySelector("#btnAdd");
-const cloneCardContainer = document.querySelector("#cloneCard");
-
-btnAdd.addEventListener("click", function() {
-    const cardToClone = cloneCardContainer.querySelector(".card");
-
-    // Verificar se a coluna "A Fazer" possui um card para clonar
-    if (cardToClone) {
-        // Exibir um prompt para o usuário inserir o título
-        const title = prompt("Digite o título:");
-
-        // Verificar se o usuário inseriu um título
-        if (title) {
-            const cloneCard = cardToClone.cloneNode(true);
-
-            // Atualizar o título do card clonado com o valor inserido pelo usuário
-            const titleElement = cloneCard.querySelector("h2");
-            titleElement.textContent = title;
-
-            cloneCardContainer.appendChild(cloneCard);
-
-            // Adicionar ouvintes de eventos de arrastar e soltar nos cards clonados
-            cloneCard.addEventListener("dragstart", dragstart);
-            cloneCard.addEventListener("drag", drag);
-            cloneCard.addEventListener("dragend", dragend);
-            cloneCard.addEventListener("drop", drop);
-        }
+function addText() {
+    let inputText = document.getElementById("inputText").value;
+  
+    if (inputText) {
+      const cardToClone = cloneCardContainer.querySelector(".card");
+      
+      if (cardToClone) {
+        const cloneCard = cardToClone.cloneNode(true);
+        const titleElement = cloneCard.querySelector("h2");
+        titleElement.textContent = inputText;
+        cloneCardContainer.appendChild(cloneCard);
+  
+        cloneCard.addEventListener("dragstart", dragstart);
+        cloneCard.addEventListener("drag", drag);
+        cloneCard.addEventListener("dragend", dragend);
+        cloneCard.addEventListener("drop", drop);
+      }
     }
-});
 
+    let popup = document.getElementById("popup");
+    popup.style.display = "none";
 
+    document.getElementById("inputText").value = "";
+  }
+  
+  const btnAdd = document.querySelector("#btnAdd");
+  const cloneCardContainer = document.querySelector("#cloneCard");
+  
+  btnAdd.addEventListener("click", openPopup);
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    let addButton = document.getElementById("addButton");
+  
+    addButton.addEventListener("click", addText);
+  });
+
+// const btnAdd = document.querySelector("#btnAdd");
+// const cloneCardContainer = document.querySelector("#cloneCard");
+
+// btnAdd.addEventListener("click", function() {
+//     const cardToClone = cloneCardContainer.querySelector(".card");
+
+//     // Verificar se a coluna "A Fazer" possui um card para clonar
+//     if (cardToClone) {
+//         // Exibir um prompt para o usuário inserir o título
+//         const title = prompt("Digite o título:");
+
+//         // Verificar se o usuário inseriu um título
+//         if (title) {
+//             const cloneCard = cardToClone.cloneNode(true);
+
+//             // Atualizar o título do card clonado com o valor inserido pelo usuário
+//             const titleElement = cloneCard.querySelector("h2");
+//             titleElement.textContent = title;
+
+//             cloneCardContainer.appendChild(cloneCard);
+
+//             // Adicionar ouvintes de eventos de arrastar e soltar nos cards clonados
+//             cloneCard.addEventListener("dragstart", dragstart);
+//             cloneCard.addEventListener("drag", drag);
+//             cloneCard.addEventListener("dragend", dragend);
+//             cloneCard.addEventListener("drop", drop);
+//         }
+//     }
+// });
+
+// function openPopup() {
+//     var popup = document.getElementById("popup");
+//     popup.style.display = "block";
+//   }
+  
+//   function addText() {
+//     var inputText = document.getElementById("inputText").value;
+//     console.log("Texto inserido: " + inputText);
+//     var popup = document.getElementById("popup");
+//     popup.style.display = "none";
+//   }
+  
+//   document.addEventListener("DOMContentLoaded", function() {
+//     var openButton = document.getElementById("openButton");
+//     var addButton = document.getElementById("addButton");
+  
+//     openButton.addEventListener("click", openPopup);
+//     addButton.addEventListener("click", addText);
+//   });
+  
 
 // const btnAdd = document.getElementById("btnAdd")
 // btnAdd.addEventListener("click", addNew)
