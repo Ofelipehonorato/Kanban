37462e03382c2f6e495be2251f5fe2621bdf3d11
Felipe Franciscato Honorato <fefranciscato@gmail.com>
2023-06-26 10:20:32 -0300

    Configuração do botão editar título.

---
+++ js/buttons.js
@@ -0,0 +1,63 @@
+import { dragstart, drag, dragend, drop } from "./dropAndDrag.js";
+
+// Função que abre um campo para inserir o titulo do card //
+function openPopup() {
+    let popup = document.getElementById("popup");
+    popup.style.display = "block";
+  }
+
+// Função para adicionar um novo card //
+function addText() {
+    let inputText = document.getElementById("inputText").value;
+  
+    if (inputText) {
+      const cardToClone = cloneCardContainer.querySelector(".card");
+      
+      if (cardToClone) {
+        const cloneCard = cardToClone.cloneNode(true);
+        const titleElement = cloneCard.querySelector("h2");
+        titleElement.textContent = inputText;
+        cloneCardContainer.appendChild(cloneCard);
+  
+        cloneCard.addEventListener("dragstart", dragstart);
+        cloneCard.addEventListener("drag", drag);
+        cloneCard.addEventListener("dragend", dragend);
+        cloneCard.addEventListener("drop", drop);
+      }
+    }
+
+    let popup = document.getElementById("popup");
+    popup.style.display = "none";
+
+    document.getElementById("inputText").value = "";
+  }
+  
+  const btnAdd = document.querySelector("#btnAdd");
+  const cloneCardContainer = document.querySelector("#cloneCard");
+  
+  btnAdd.addEventListener("click", openPopup);  
+  document.addEventListener("DOMContentLoaded", function() {
+    let addButton = document.getElementById("addButton");
+    addButton.addEventListener("click", addText);
+  });
+
+// Função que insere o nome no titulo do kanban //
+
+function editarNome(){
+  let titulo = document.getElementById("tituloEdit")
+  let novoTitulo = prompt("Insira o nome do seu kanban")
+
+  if(novoTitulo && novoTitulo.trim() !== ''){
+    titulo.innerHTML = novoTitulo
+  } else {
+    alert("O titulo não pode ficar vazio!!")
+  }
+}
+var btnedit = document.getElementById("btnedit")
+btnedit.addEventListener("click", editarNome)
+
+
+
+
+
+
