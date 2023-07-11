ae22f1ada3a41d828cb9fe7106b4afc243eb2a42
Felipe Franciscato Honorato <50453449+Ofelipehonorato@users.noreply.github.com>
2023-07-11 09:04:24 -0300

    bug

---
+++ js/addCard.js
@@ -0,0 +1,84 @@
+import { dragstart, drag, dragend, drop } from "./dropAndDrag.js";
+
+function openPopup() {
+  let popup = document.getElementById("popup");
+  popup.style.display = "block";
+  const input = document.getElementById("inputText")
+  input.value = "";
+};
+
+function closePopup() {
+  let popup = document.getElementById("popup");
+  popup.style.display = "none";
+};
+
+const btnPopUp = document.querySelector("#btnAdd");
+btnPopUp.addEventListener("click", openPopup);
+
+const btnClosePopUp = document.querySelector("#closePopup");
+btnClosePopUp.addEventListener("click", closePopup);
+
+
+const btnAdd = document.getElementById("addButton");
+btnAdd.addEventListener("click", criarCard);
+
+let cardsArray = [];
+function criarCard() {
+
+  console.log(cardsArray)
+
+  let popup = document.getElementById("popup");
+  popup.style.display = "none";
+  
+  const input = document.getElementById("inputText")
+  const value = input.value;
+
+  if (!value) {
+    return alert("Insira um título por favor.")
+  }
+
+  // Criação do cards e suas propriedades
+  const cardElement = document.createElement("div");
+  cardElement.classList.add("card", "afazer");
+  cardElement.id = "afazer";
+  cardElement.draggable = true;
+
+  const ulCard = document.createElement("ul");
+  ulCard.classList.add("status", "red");
+  ulCard.id = "statusRed";
+
+  const liCard = document.createElement("li");
+  liCard.classList.add("content", "afazer");
+  liCard.id = "contentAfazerId";
+  ulCard.appendChild(liCard);
+
+  const cardElementText = document.createElement("p");
+  cardElementText.id = "textoTitulo";
+  cardElementText.innerHTML = value;
+
+  liCard.appendChild(cardElementText);
+  cardElement.appendChild(ulCard);
+  cardsArray.push(cardElement);
+
+  let kanbanContainer = document.getElementById("dropzone");
+  kanbanContainer.appendChild(cardElement);
+
+  cardElement.addEventListener("dragstart", dragstart);
+  cardElement.addEventListener("drag", drag);
+  cardElement.addEventListener("dragend", dragend);
+  cardElement.addEventListener("drop", drop);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("btnDelete");
+  deleteButton.innerText = "Apagar";
+  liCard.appendChild(deleteButton);
+
+  // Adicionar evento de exclusão ao botão
+  deleteButton.addEventListener("click", function() {
+    alert("Tarefa removida!!")
+    cardElement.remove();
+  });
+
+
+}
+
